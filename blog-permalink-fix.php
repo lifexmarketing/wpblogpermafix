a16491bfd79204d72ce777dc1e7579a6d8d02a53
Ryan <sb7audits@gmail.com>
2026-04-22 11:18:14 -0400

    Convert to regular plugin with GitHub update checking via PUC
    - Adds Plugin Update Checker v5.6 (vendored) so WordPress shows update
      notices when a new GitHub release is tagged
    - Adds GitHub Actions workflow that builds and attaches a release zip
      whenever a v* tag is pushed

--- blog-permalink-fix.php
+++ blog-permalink-fix.php
@@ -1,9 +1,22 @@
 <?php
 /**
  * Plugin Name: Blog Permalink Fix
+ * Plugin URI:  https://github.com/lifexmarketing/wpblogpermafix
  * Description: Prevents custom post types from inheriting the /blog/ permalink front. Only built-in posts use /blog/%postname%/.
- * Version: 1.0.0
+ * Version:     1.0.0
+ * Author:      Lifex Marketing
+ * License:     GPL-2.0-or-later
  */
+
+require_once __DIR__ . '/vendor/plugin-update-checker/plugin-update-checker.php';
+
+use YahnisElsts\PluginUpdateChecker\v5p6\PucFactory;
+
+$updateChecker = PucFactory::buildUpdateChecker(
+	'https://github.com/lifexmarketing/wpblogpermafix/',
+	__FILE__,
+	'blog-permalink-fix'
+);
 
 add_filter( 'register_post_type_args', function( $args, $post_type ) {
 	$built_in = [ 'post', 'page', 'attachment', 'revision', 'nav_menu_item' ];
